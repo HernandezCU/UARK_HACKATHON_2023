ecfea931d2278d66702ec2636db8eff35850f7f6
robPTY <rcadown8@gmail.com>
2023-03-31 21:06:44 -0500

    set up firebase

--- __init__.py
+++ __init__.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Home page of our python application.'
+
+@app.route('/search') # Endpoint for whenever user searches for a place
+def search():
+    location = request.args.get('location')
+    radius = request.args.get('radius', default=75)
+    type = request.args.get('type', default='park')
+    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    params = {'location': location, 'radius': radius, 'type': type, 'key': 'YOUR_API_KEY'}
+    response = requests.get(url, params=params)
+    data = response.json()
+    if data['status'] == 'OK':
+        places = []
+        for result in data['results']:
+            places.append({'name': result['name'], 'address': result['vicinity'], 'rating': result.get('rating', '-')})
+        return jsonify({'results': places})
+    else:
+        return jsonify({'error': 'Unable to search for places'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
